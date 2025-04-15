d5ac452f3b6d827f53228c8735a57f1dc827e872
sampreeth arigala <sampreetharigala@gmail.com>
2025-04-15 03:44:06 +0000

    commit

--- models/fact_order.sql
+++ models/fact_order.sql
@@ -1,4 +1,3 @@
-
 with orders as  (
     select * from {{ ref('stg_orders' )}}
 ),
@@ -10,7 +9,7 @@ payments as (
 order_payments as (
     select
         order_id,
-        sum(case when status = 'success' then amount end) as amount
+        sum(case when status = 'completed' then amount end) as amount
 
     from payments
     group by 1
